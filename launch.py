08363cde72c0519832aaf43b237cc4893338d462
Johannes Plapp <johannes@plapp.de>
2017-10-13 10:33:54 +0200

    improved launcher

--- launch.py
+++ launch.py
@@ -1,34 +1,35 @@
 # todo move pcs to separate file
-import subprocess
+import subprocess, time
 
 clients = [
-'atcremers14'
-'atcremers16'
-'atcremers45'
-'atcremers48'
-'atcremers49'
-'atcremers50'
-'atcremers51'
-'atcremers52'
-'atcremers53'
-'atcremers54'
-'atcremers55'
-'atcremers57'
-'atcremers58'
-'atcremers59'
-'atcremers60'
-'atcremers61'
-'atcremers62'
-'atcremers63'
-'atcremers64'
-'atcremers65'
-'atcremers66'
-'atcremers75'
-'atcremers76'
+'atcremers14',
+'atcremers16',
+'atcremers45',
+'atcremers48',
+'atcremers49',
+'atcremers50',
+'atcremers51',
+'atcremers52',
+'atcremers53',
+'atcremers54',
+'atcremers55',
+'atcremers57',
+'atcremers58',
+'atcremers59',
+'atcremers60',
+'atcremers61',
+'atcremers62',
+'atcremers63',
+'atcremers64',
+'atcremers65',
+'atcremers66',
+'atcremers75',
+'atcremers76',
 ]
 
-TEMPLATE = "ssh {} bash -c ' cd runnn && cd runnn && nohup python3 client.py & '"
+TEMPLATE = "ssh -o StrictHostKeyChecking=no {} bash -c ' cd runnn && killall -9 python3 ; cd runnn && nohup python3 client.py & '"
 
 for client in clients:
-   res = subprocess.getoutput(TEMPLATE.format(client))
-   print('launched on {}'.format(client), res)
+   subprocess.Popen(["bash", "-c", TEMPLATE.format(client)])
+   print('launched on {}'.format(client))
+   time.sleep(2)
